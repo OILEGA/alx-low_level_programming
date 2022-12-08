02a6fb4827b5b31aa09a82db92a3cdd213f7feca
Omena Egbusiri <get2mena@gmail.com>
2022-12-08 08:51:36 -0800

    writting our first task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main -The main function of this program is to use the puts function to print 
+ * a line of code.
+ * Return: 0
+ */
+int main(void)
+										{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+
+										}
