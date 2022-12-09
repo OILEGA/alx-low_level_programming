2a49589af9a9b3ef4320de0a883ce0e909bd38cf
Omena Egbusiri <get2mena@gmail.com>
2022-12-09 08:33:34 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main -prints alphabets in lower and upper
+ * Return:0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
