c17ff6674b6c38cf13fbbf971708374d5e6ca5cc
Omena Egbusiri <get2mena@gmail.com>
2022-12-09 10:46:03 -0800

    single digits program

---
+++ 6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits in base 10
+ * Return:0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar ((n % 10) + '0');
+
+	putchar("\n")
+
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+My first task on C Hello World
