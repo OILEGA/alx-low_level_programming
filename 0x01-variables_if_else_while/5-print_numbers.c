3b9e17d3588da49ac032bff7b053e6a7a6e16ffb
Omena Egbusiri <get2mena@gmail.com>
2022-12-09 09:54:31 -0800

    printing numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - prints all strings
+ *  Return:0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+
+	printf("\n");
+
+	return (0);
+}
