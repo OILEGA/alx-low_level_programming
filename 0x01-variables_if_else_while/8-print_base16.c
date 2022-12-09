4445053c361cb68c1360446f19432c2c490c9ee4
Omena Egbusiri <get2mena@gmail.com>
2022-12-09 13:26:26 -0800

    printing alphabet in reverse

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers to base 16 in lower case
+ * Return:0
+*/
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num < 10; num++)
+		putchar ((num % 10) + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+
+
