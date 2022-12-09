793e951af1d0836fb69e9b4a7729181aeceae230
Omena Egbusiri <get2mena@gmail.com>
2022-12-09 09:36:30 -0800

    printing alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -printing alphabets with e and q
+ *Return:0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'a' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
