cd3c23041a5ab362d2bb545cfb903d4acf47b505
Omena Egbusiri <get2mena@gmail.com>
2022-12-09 18:24:15 -0800

    printing exact

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- *main -Entry
+ *main -print an exact form
  *Return: 1
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	fprintf(stderr, "and that piece of art is useful\"-
+	\
+	Dora Korpar,2015-10-19\n");
+	return (0);
 }
 
