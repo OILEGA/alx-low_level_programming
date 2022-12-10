7de91f27438522c3dc58900dc5555c35f1e0d1ce
Omena Egbusiri <get2mena@gmail.com>
2022-12-09 17:29:20 -0800

    combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,16 @@ int main(void)
 {
 	int n;
 
-	for (a = 48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar (n);
 		if (n != 57)
 		{
 			putchar (',');
 			putchar (' ');
+		
 		}
 	putchar('\n');
+}
 	return (0);
 }
