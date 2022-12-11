ff4280b9cf4b77b3f6edee072a44aac9532c6ff5
Omena Egbusiri <get2mena@gmail.com>
2022-12-11 07:02:12 -0800

    printing combination of 2 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,17 @@
 */
 int main(void)
 {
-	for (int i = 0; i < 8; i++)
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = i + 1; j < 9; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			for (int k = j + 1; k < 10; k++)
-			{
-				putchar (i + '0');
-				putchar (j + '0');
-				putchar (k + '0');
-				putchar (' ');
-			}
+			putchar (i % 10 + '0');
+			putchar (j % 10 + '0');
+			if (i == 8 && j == 8)
+			putchar (',');
+			putchar (' ');
 		}
 	}
 	return (0);
