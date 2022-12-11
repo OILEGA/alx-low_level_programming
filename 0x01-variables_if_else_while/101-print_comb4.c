b4a38be5d70342841acfb60c29223dc61cb17e7a
Omena Egbusiri <get2mena@gmail.com>
2022-12-11 10:18:41 -0800

    combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,10 +21,11 @@ int main(void)
 					continue;
 				if (i == j && i == k)
 					continue;
-			putchar (i % 10 + '0');
-			putchar (j % 10 + '0');
-			putchar (k % 10 + '0');
-			if (i == 7 && j == 8 && k == 9)
+
+				putchar (i % 10 + '0');
+				putchar (j % 10 + '0');
+				putchar (k % 10 + '0');
+				if (i == 7 && j == 8 && k == 9)
 					continue;
 				putchar (',');
 				putchar (' ');
