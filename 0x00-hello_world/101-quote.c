0fb32bb9f2a3dab492f4c3f84fddc22d0566d066
Omena Egbusiri <get2mena@gmail.com>
2022-12-11 12:54:33 -0800

    print exact statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main -print an exact form
- *@void:parameter
  *Return: 1 (success)
 */
 int main(void)
 {
-	fprint (2, "and that piece of art is useful\"- Dora
+
+	write (2, "and that piece of art is useful\"- Dora
 		   Korpar,2015-10-19\n", 59);
 	return (1);
 }
