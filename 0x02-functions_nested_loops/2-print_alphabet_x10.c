8d9268395fb45a0c3ad2560b51780bba89283682
Omena Egbusiri <get2mena@gmail.com>
2022-12-13 10:25:04 -0800

    printing alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * void print_alphabet_x10(void) - print alphabets 10 times
+ * print_alphabet_x10(void) - print alphabets 10 times
  */
 void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
 
-	while (count++ <= 9
+	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z' ; letter++)
-			_putchar(letter)
+			_putchar(letter);
 		_putchar('\n');
+	}
 }
