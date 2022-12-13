beba22b8a8f1fddf3f66102094b60e4faad46cb8
Omena Egbusiri <get2mena@gmail.com>
2022-12-13 12:25:56 -0800

    checks for alphabetic order

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _isalpha - to check for char alphabets
+ * @c: the char to be checked
+ *
+ * Return: 1 if char is lowercase or uppercase, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'z'))
+		return (1);
+	else 
+		return (0);
+
+}
