74814b46e667a6fd57f2a71d15cd599e0b8dcf32
Omena Egbusiri <get2mena@gmail.com>
2022-12-13 14:24:09 -0800

    absolute valueE

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute values
+ * @n: the integer to be inputed
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+
+}
