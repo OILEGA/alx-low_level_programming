c7992dcf94664ca4839da14e85c1e040c45011b3
Omena Egbusiri <get2mena@gmail.com>
2022-12-13 12:36:50 -0800

    checked for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  */
 int _isalpha(int c)
 {
-	if((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
 		return (1);
-	else 
+	else
 		return (0);
 
 }
