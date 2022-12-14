fb5ba2eacb07bc7efcc82f0646cea7253ea237a6
Omena Egbusiri <get2mena@gmail.com>
2022-12-14 07:21:08 -0800

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,14 +22,14 @@ void print_times_table(int n)
 		_putchar(32);
 		_putchar(op + 48);
 		}
-		else if (op <= 99)
+		else (op <= 99)
 
 		{
 		_putchar(32);
 		_putchar((op / 10) + 48);
 		_putchar((op % 10) + 48);
 		}
-		else
+		else if
 		{
 		_putchar(((op / 100) % 10) + 48);
 		_putchar(((op / 10) % 10) + 48);
