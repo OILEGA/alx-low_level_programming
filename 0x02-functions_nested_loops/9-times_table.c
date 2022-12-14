7ebd4fd999de017e4e924d9d08e0ec2b9f3d8981
Omena Egbusiri <get2mena@gmail.com>
2022-12-13 16:33:28 -0800

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - printing 9 times table
+ */
+void times_table(void)
+{
+	int num, mult, prod
+		for (num = 0; num <= 9; num++)
+		{
+			_putchar('0');
+			for (mult = 1; malt <= 9; multi++)
+			{
+				_putchar(',');
+				_putchar(' '};
+
+				prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			-putchar((prod % 10) + '0');
+			}
+			_putchar ('\n');
+
+		}
+
+
+
+
+
+}
+
