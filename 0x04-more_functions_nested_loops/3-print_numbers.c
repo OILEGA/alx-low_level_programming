26c412c5854fd7cd4e20d15cf3792e63ab670174
Omena Egbusiri <get2mena@gmail.com>
2022-12-15 19:40:02 -0800

    printing numbers from zeo to nin

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+/**
+ * print_numbers - prints number from zero to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar ('\n');
+}
+
+
