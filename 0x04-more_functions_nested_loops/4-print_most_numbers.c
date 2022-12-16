f10e4504584c57a1d9e1d68d2ef76052d9a5ac8c
Omena Egbusiri <get2mena@gmail.com>
2022-12-15 19:58:21 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0' c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2; || c == '4'))
+		if (!(c == '2 || c == '4'))
 			_putchar(c);
 	}
 	_putchar('\n');
