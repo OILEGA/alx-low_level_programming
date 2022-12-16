4dddd322f52fa06ec72ad9e75252ffcd5f010526
Omena Egbusiri <get2mena@gmail.com>
2022-12-15 19:04:58 -0800

    checking for upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 
 /**
  * _isupper - checks if parameter is an uppercase character.
- * @c: input character
+ * @c: to check character
  * Return: 0 or 1
 */
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'z')
 		return (1);
