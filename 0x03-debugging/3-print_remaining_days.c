1e4a4b5370d91a30ecb3104b8de2e6c9ad2d07bf
Omena Egbusiri <get2mena@gmail.com>
2022-12-16 20:11:33 -0800

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,13 +10,16 @@
 * Return: void
 */
 
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year);
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+
+	if
+		((year % 400 == 0) || (year % 4 == 0)
+	&& year % 100 != 0))
 	{
-		if (month >= 2 && day >= 60)
+	if (month >= 3 && day >= 60)
 	{
-		day++;
+	day++;
 	}
 
 	printf("Day of the year: %d\n", day);
@@ -24,13 +27,14 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-	if (month == 2 && day == 60)
+		if (month == 2 && day == 60)
 	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year)
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		else
 	{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
+		printf("Day of the year: %d\n", day);
+
+		printf("Remaining days: %d\n", 365 - day);
 	}
 	}
 	}
