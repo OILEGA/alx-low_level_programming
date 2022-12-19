f2ec69171b8a24544349d090f6f5caae5411dbfe
Omena Egbusiri <get2mena@gmail.com>
2022-12-19 13:50:17 -0800

    prints a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string then a new line
+ *@str: the string we are to print out
+ *Return: length of the string
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
