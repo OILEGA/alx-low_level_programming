cf8fd4f8e9afd7a821e83a111b300a0246fc0367
Omena Egbusiri <get2mena@gmail.com>
2022-12-19 14:39:49 -0800

    prints a string in reverse direction

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints string in rev
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	-putchar('\n');
+}
