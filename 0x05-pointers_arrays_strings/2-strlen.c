53f3f5eb450f1759dbc00e28b24bd78555ca4f69
Omena Egbusiri <get2mena@gmail.com>
2022-12-19 13:35:50 -0800

    returning the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,7 +10,7 @@ int _strlen(char *s)
 	int a = 0;/* this is where we start our counting from, from 0*/
 
 	for (; *s++;) /* using a for loop to count our 6 strings, s to g*/
-		(a++;)/* to run our increment starting from 0*/
+		a++;/* to run our increment starting from 0*/
 	return (a);
 
 }
