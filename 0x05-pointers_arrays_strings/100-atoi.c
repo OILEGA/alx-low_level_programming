437aac900f2d071071ab29f09cb641072650ba29
Omena Egbusiri <get2mena@gmail.com>
2022-12-19 20:59:30 -0800

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,40 +2,41 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: input string
+ * @s: input string to convert
  * Return: integer
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int i;
+	int n = 0;
+	int signo = 1;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
+	while ((s[i] < '0' || s[i] > '9')
+	&& s[i] != 0)
 	{
-		if (s[i] == '-')
-			++d;
-	if (s[i] >= '0' && s[i] <= '9'
+		if (s[i] == -1)
+			signo *= -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9')
+	&& s[i] != 0)
 	{
-		digit = s[i] - '0';
-		if (d % 2)
-		digit = -digit;
-		n = n * 10 + digit;
-		f = 1;
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
-		f = 0;
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++
+		}
 	}
-	i++;
-	}
-	if (f == 0)
-	return (0);
-	return (n);
+	signo *= -1;
+	return (n * signo);
 }
+
+
+
+
+
