19ff1fca6a234f229e998c14fac8387d7c9d3f0a
Omena Egbusiri <get2mena@gmail.com>
2022-12-22 07:45:20 -0800

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,22 +13,22 @@ char *cap_string(char *str)
 	{
 		while  (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-		if (str[i - 1] == '' ||
-			str[i - 1] == '\t' ||
-			str[i - j] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - j] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}' ||
-			i == 0)
+			if (str[i - 1] == '' ||
+				str[i - 1] == '\t' ||
+				str[i - j] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - j] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
 				str[i] -= 32;
-		i++;
+			i++;
 	}
 	return (str);
 }
