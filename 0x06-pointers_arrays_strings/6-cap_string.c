7b0c96d9c028d6de9518bcd0a91c93f7019c0006
Omena Egbusiri <get2mena@gmail.com>
2022-12-22 08:20:11 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,12 +7,13 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int index = 0;
 
-	while (str[i])
+	while (str[index])
 	{
-		while  (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
+		while  (!(str[index] >= 'a' && str[index] <= 'z'))
+			point++;
+
 			if (str[i - 1] == '' ||
 				str[i - 1] == '\t' ||
 				str[i - j] == '\n' ||
@@ -26,9 +27,9 @@ char *cap_string(char *str)
 				str[i - 1] == ')' ||
 				str[i - 1] == '{' ||
 				str[i - 1] == '}' ||
-				i == 0)
-				str[i] -= 32;
-			i++;
+				point == 0)
+				str[point] -= 32;
+			point++;
 	}
 	return (str);
 }
