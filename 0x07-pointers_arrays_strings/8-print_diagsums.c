9e8afe1bcd184aced916a585f73bdc58f509d249
Omena Egbusiri <get2mena@gmail.com>
2023-01-02 14:58:53 -0800

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,19 +9,19 @@
 void print_diagsums(int *a, int size)
 {
 
-	int x1, x2, y;
+	int sum1, sum2, y;
 
-	x1 = 0;
-	x2 = 0;
+	sum1 = 0;
+	sum2 = 0;
 	for (y = 0; y < size; y++)
 	{
-		x1 = x2 + a[y * size + y];
+		sum1 = sum2 + a[y * size + y];
 	}
 	for (y = size - 1; y >= 0; y--)
 	{
-		x2 += a[y * size * (size - y - 1)];
+		sum2 += a[y * size * (size - y - 1)];
 	}
-	printf("%d, %d\n", x1, x2);
+	printf("%d, %d\n", sum1, sum2);
 }
 
 
