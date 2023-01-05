1ee762bba0cfa0d48efa8d70fdf0ef775427e4e7
Omena Egbusiri <get2mena@gmail.com>
2023-01-05 06:36:42 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,29 @@
 #include "main.h"
+
 /**
- * check- checks for the square root of a number
- * @a: int
- * @b: int
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ *
  * Return: int
  */
-int check ( int a, int b)
+int check(int a, int b)
 {
 	if (a * a == b)
-	
 		return (a);
-	
-	if (a * a > b )
-
+	if (a * a > b)
 		return (-1);
-	
 	return (check(a + 1, b));
 }
-/**
- *  _sqrt_recursion - returns the naturals sqr root of a number
- *  @n: to find sqr root of
- *  Return: nat sqr root of or -1
- */
 
-	int _sqrt_recursion(int n)
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
 {
-	if ( n == 0)
+	if (n == 0)
 		return (0);
 	return (check(1, n));
 }
