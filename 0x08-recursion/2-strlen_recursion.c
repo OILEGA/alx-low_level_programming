326d3a2457fde1fe69b88708d620b3a854e12293
Omena Egbusiri <get2mena@gmail.com>
2023-01-04 22:47:30 -0800

    returning length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - used to return length of string
+ * @s: string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
