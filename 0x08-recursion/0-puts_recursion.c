39f014ad5b1dffe464ea4455d5cceebe13ea1ae7
Omena Egbusiri <get2mena@gmail.com>
2023-01-04 22:13:16 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line.
- * @s: istring
+ * @s: string
  * Return: no return
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 
 	{
 		_putchar(*s);
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
+int _putchar(char)
 void _puts_recursion(char *s)
 void _print_rev_recursion(char *s)
 int _strlen_recursion(char *s)
