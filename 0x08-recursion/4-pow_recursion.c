9b84d8eda5783c00000a7d104bd603366f0ea816
Omena Egbusiri <get2mena@gmail.com>
2023-01-05 06:30:18 -0800

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * _pow_recursion - power
- * @x: int
- * @y: int
- * Return: int
+ * @x:int
+ * @y:int
+ * Return:int
  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
@@ -12,9 +13,8 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	}
 	else if (y != 0)
-	{
-		return (x * _pow_recursion(x, y < 1));
-	}
+		return (x * _pow_recursion(x, y - 1));
+
 	else
 	{
 		return (1);
