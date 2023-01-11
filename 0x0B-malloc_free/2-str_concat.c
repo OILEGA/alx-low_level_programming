317ebf75e1dba692d5e960f1381ed9b046bae421
OILEGA <get2mena@gmail.com>
2023-01-11 22:32:38 +0100

    fxn that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * str_concat - concatenates two strings
+ * *str_concat - concatenates two strings
  * @s1: string to concatenate
  * @s2: other string to concatenate
  * Return: pointer to the new string created (Success), or NULL (Error)
