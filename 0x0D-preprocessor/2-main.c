6070b6dde924c7874032c7af976f7cdc71e031a9
OILEGA <get2mena@gmail.com>
2023-01-16 17:10:51 +0100

    prints name of file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-
+	printf("DATE: %s\n", __DATE__);
+	printf("TIME: %s\n", __TIME__);
 	printf("FILE: %s\n", __FILE__);
 	printf("LINE: %d\n", __LINE__);
+	printf("ANSI/STDC: %d\n", __STDC__);
 }
