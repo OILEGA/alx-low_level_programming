02b86a06b8e82479a549b4a59ad7262255122462
OILEGA <get2mena@gmail.com>
2023-01-18 00:03:42 +0100

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,7 +64,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
-		return (NULL)
+		return (NULL);
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 
 	if (dog->name == NULL)
