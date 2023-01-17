66c35a18617074cc110e879e93de049e9c559692
OILEGA <get2mena@gmail.com>
2023-01-17 22:01:38 +0100

    a fxn that initializes a variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - is used to initialize a variable
@@ -11,8 +11,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
+	{
 		d = malloc(sizeof(struct dog));
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
 }
