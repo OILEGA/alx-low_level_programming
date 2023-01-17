d027f1c58a243ce73126d5e45418dfca11a3590d
OILEGA <get2mena@gmail.com>
2023-01-17 22:22:10 +0100

    a fxn that frees dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_dog - frees the memory allocated for struct dog.
+ * @d: struct dog we are to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
