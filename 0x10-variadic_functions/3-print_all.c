093f9bf24ab3b9d8905d87a79f434a7796aa12f8
OILEGA <get2mena@gmail.com>
2023-01-20 19:08:37 +0100

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *  print_all - to print answer to stdout
+ * @format: list of types of arguments passed to
+ * the fxn
+ * description: if string is NULL, print nil otherwise ignore
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *sep = "";
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+				printf("%s%c", sep, va_arg(list, int));
+				break;
+				case 'i':
+				printf("%s%d", sep, va_arg(list, int));
+				break;
+				case 'f':
+				printf("%s%f", sep, va_arg(list, double));
+				break;
+				case 's':
+				str = va_arg(list, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+				default:
+				i++;
+				continue;
+			}
+			sep = ",";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
