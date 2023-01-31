30e58a6fa9db49f552799257f9a4a5da9e03a8e8
OILEGA <get2mena@gmail.com>
2023-01-31 21:23:54 +0100

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 /**
  * sum_listint - calculates the sum of all the data in a listint_t list
- * @head:
- * first node in the linked list
+ * @head: first node in the linked list
  *
  * Return: the sum of all the numbers
  */
