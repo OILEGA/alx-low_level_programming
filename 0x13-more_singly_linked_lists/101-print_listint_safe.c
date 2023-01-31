e1af1b9737319dd04f9beab7feff6ccca59c2c8e
OILEGA <get2mena@gmail.com>
2023-01-31 21:33:23 +0100

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,7 +9,7 @@ size_t print_listint_safe(const listint_t *head);
  * Return: 0 if the list is not looped, otherwise
  * the number of unique nodes in the list
  */
-size_t print_listint_safe(const listint_t *head)
+size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
@@ -50,6 +50,7 @@ size_t print_listint_safe(const listint_t *head)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes, index = 0;
+	
 	nodes = looped_listint_len(head);
 	if (nodes == 0)
 	{
