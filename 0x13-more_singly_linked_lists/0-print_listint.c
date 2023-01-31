b3c7558eae0b673ef6de11ee6baec22f31cb0bcf
OILEGA <get2mena@gmail.com>
2023-01-31 11:29:14 +0100

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t numNode = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		numNode++;
+		num++;
 		h = h->next;
 	}
-	return (numNode);
+	return (num);
 }
