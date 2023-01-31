6514241c6c66fe4b50d7f831435af431b52ff891
OILEGA <get2mena@gmail.com>
2023-01-31 17:33:15 +0100

    Write a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -16,18 +16,20 @@ size_t free_listint_safe(listint_t **h)
 	{
 		diff = *h - (*h)->next;
 	if (diff > 0)
-		{temp = (*h)->next;
-		free(*h);
-		*h = temp;
-		len++;
-		}
+	{
+	temp = (*h)->next;
+	free(*h);
+	*h = temp;
+	len++;
+	}
 
 	else
-		{free(*h);
-		*h = NULL;
-		len++;
+	{
+	free(*h);
+	*h = NULL;
+	len++;
 	break;
-		}
+	}
 	}
 	*h = NULL;
 	return (len);
